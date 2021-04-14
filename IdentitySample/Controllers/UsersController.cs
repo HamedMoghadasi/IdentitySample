dffdd587ead05f9537b11b82ab92409bed3900bd
Hamed Moghadasi <h4lmed@gmail.com>
2021-04-14 15:04:37 +0430

    Add project files.

---
+++ IdentitySample/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentitySample.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IEnumerable<IdentityUser> Get()
+        {
+            return _userManager.Users.AsEnumerable();
+        }
+
+        public IEnumerable<>
+    }
+}
